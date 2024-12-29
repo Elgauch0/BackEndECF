163e43b4f98d2ea5c74fa0610904b24c46c91b76
Anass El kaouri <elkaourianass@gmail.com>
2024-12-29 21:05:15 +0100

    adding fixtures

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class AppFixtures extends Fixture
+{
+    private UserPasswordHasherInterface $passwordHasher;
+
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->passwordHasher = $passwordHasher;
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        $admin = new User();
+        $hashedPassword = $this->passwordHasher->hashPassword($admin, 'password');
+        $admin->setEmail('admin@admin.com')
+            ->setNom('nom admin')
+            ->setPrénom('prenom admin')
+            ->setRoles(['Admin_Role'])
+            ->setPassword($hashedPassword);
+
+        $veterinaire = new User();
+        $hashedPassword = $this->passwordHasher->hashPassword($veterinaire, 'password');
+        $veterinaire->setEmail('veterinaire@vet.com')
+            ->setPrénom('prenom vet')
+            ->setNom('nom vet')
+            ->setRoles(['Veterinaire_Role'])
+            ->setPassword($hashedPassword);
+
+        $employe = new User();
+        $hashedPassword = $this->passwordHasher->hashPassword($employe, 'password');
+        $employe->setEmail('employe@emp.com')
+            ->setPrénom('prenom employe')
+            ->setNom('nom employe')
+            ->setPassword($hashedPassword);
+
+        $manager->persist($admin);
+        $manager->persist($veterinaire);
+        $manager->persist($employe);
+        $manager->flush();
+    }
+}
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -20,17 +20,23 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column(length: 180)]
     private ?string $email = null;
 
-    /**
-     * @var list<string> The user roles
+    /** 
+     * @var list<string> The user roles 
      */
     #[ORM\Column]
     private array $roles = [];
 
-    /**
-     * @var string The hashed password
+    /** 
+     * @var string The hashed password 
      */
     #[ORM\Column]
     private ?string $password = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $prénom = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $nom = null;
 
     public function getId(): ?int
     {
@@ -45,46 +51,43 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function setEmail(string $email): static
     {
         $this->email = $email;
-
         return $this;
     }
 
-    /**
-     * A visual identifier that represents this user.
-     *
-     * @see UserInterface
+    /** 
+     * A visual identifier that represents this user. 
+     * 
+     * @see UserInterface 
      */
     public function getUserIdentifier(): string
     {
         return (string) $this->email;
     }
 
-    /**
-     * @see UserInterface
-     *
-     * @return list<string>
+    /** 
+     * @see UserInterface 
+     * 
+     * @return list<string> 
      */
     public function getRoles(): array
     {
         $roles = $this->roles;
         // guarantee every user at least has ROLE_USER
-        $roles[] = 'ROLE_USER';
-
+        $roles[] = 'ROLE_EMPLOYE';
         return array_unique($roles);
     }
 
-    /**
-     * @param list<string> $roles
+    /** 
+     * @param list<string> $roles 
      */
     public function setRoles(array $roles): static
     {
         $this->roles = $roles;
-
         return $this;
     }
 
-    /**
-     * @see PasswordAuthenticatedUserInterface
+    /** 
+     * @see PasswordAuthenticatedUserInterface 
      */
     public function getPassword(): ?string
     {
@@ -94,16 +97,37 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function setPassword(string $password): static
     {
         $this->password = $password;
-
         return $this;
     }
 
-    /**
-     * @see UserInterface
+    /** 
+     * @see UserInterface 
      */
     public function eraseCredentials(): void
     {
         // If you store any temporary, sensitive data on the user, clear it here
         // $this->plainPassword = null;
+    }
+
+    public function getPrénom(): ?string
+    {
+        return $this->prénom;
+    }
+
+    public function setPrénom(string $prénom): static
+    {
+        $this->prénom = $prénom;
+        return $this;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): static
+    {
+        $this->nom = $nom;
+        return $this;
     }
 }
