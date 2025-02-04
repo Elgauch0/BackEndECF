dad3eaa1379c9b24031186e04934444e1c8b6c52
Anass El kaouri <elkaourianass@gmail.com>
2025-02-04 22:32:01 +0100

    Avis table & controller

---
+++ src/Entity/Avis.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AvisRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: AvisRepository::class)]
+class Avis
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 60)]
+    private ?string $username = null;
+
+    #[ORM\Column(length: 500)]
+    private ?string $avis = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?bool $isValid = false;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUsername(): ?string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(string $username): static
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    public function getAvis(): ?string
+    {
+        return $this->avis;
+    }
+
+    public function setAvis(string $avis): static
+    {
+        $this->avis = $avis;
+
+        return $this;
+    }
+
+    public function isValid(): ?bool
+    {
+        return $this->isValid;
+    }
+
+    public function setValid(?bool $isValid): static
+    {
+        $this->isValid = $isValid;
+
+        return $this;
+    }
+}
